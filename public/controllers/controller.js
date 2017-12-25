59a2d2c76b5d12b20c37f0c9a6905b997fd75f85
KenJoelTL <joellutumba@hotmail.com>
2017-12-24 22:02:11 -0500

    Retrait des données mock

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -24,31 +24,4 @@ myApp.controller('AppCtrl',['$scope','$http',function($scope,$http){
   });
 };
 
-/*
-      personne1 = {
-        nom : "Joel",
-        courriel : "joel@mail.com",
-        numero : "514-321-3214"
-
-    };
-
-    personne2 = {
-        nom : "Caroline",
-        courriel : "caroline@mail.com",
-        numero : "514-321-3214"
-
-    };
-
-    personne3 = {
-        nom : "Charles",
-        courriel : "charles@mail.com",
-        numero : "514-789-3564"
-    };
-
-    var listeContacts = [personne1, personne2, personne3];
-    // la variable scope est un peu comme les viewData. c'est le lien entre la controleur et la vue
-    $scope.listeContacts = listeContacts;
-*/
-
-
 }]);
